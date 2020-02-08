cfe49cc6392b4c4fbbdc540a32a23ed8b44f232b
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-02-08 10:52:33 +0900

    implement scale anim

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -19,6 +19,7 @@ package com.google.samples.propertyanimation
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -103,7 +104,16 @@ class MainActivity : AppCompatActivity() {
         animator.start()
     }
 
+    // To animate multiple params, use PropertyValuesHolder and pass them to ObjectAnimator.
     private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f) // x4 size
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(star, scaleX, scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(scaleButton)
+        animator.start()
     }
 
     private fun fader() {
